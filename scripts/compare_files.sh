e3fd84699c814d156ea7c9fa6eb3cb6954d7bd10
Dmitry Ryumin <neweraairesearch@gmail.com>
2024-01-21 12:41:10 +0300

    Update compare_files.sh

--- scripts/compare_files.sh
+++ scripts/compare_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 SOURCE_DIR="$1"
 TARGET_DIR="$2"
-
-# Create a header for the table
-echo -e "File\t\tComparison Result"
 
 # Create an array to store files that need to be updated
 files_to_update=()
@@ -16,7 +18,11 @@ if [ -d "$TARGET_DIR" ]; then
       # Check if files differ
       if ! cmp -s "$target_file" "$file"; then
         # Print the filename and a message indicating files differ
-        echo -e "$(basename "$file")\t\tFiles differ"
+        if [ ${#files_to_update[@]} -eq 0 ]; then
+          # Create a header for the table if it's the first differing file
+          echo -e "File\t\t${RED}Comparison Result${NC}"
+        fi
+        echo -e "$(basename "$file")\t\t${RED}Files differ${NC}"
         # Display unified diff without lines starting with + or -
         colordiff -u "$target_file" "$file" | sed -n '/^[^+-]/p'
         # Update the target file
@@ -40,7 +46,8 @@ if [ ${#files_to_update[@]} -eq 0 ]; then
   echo "No files to update."
 else
   # Output the list of updated files
+  echo -e "\nUpdated files:"
   for updated_file in "${files_to_update[@]}"; do
-    echo "Updated: $updated_file"
+    echo -e "${GREEN}$updated_file${NC}"
   done
 fi
